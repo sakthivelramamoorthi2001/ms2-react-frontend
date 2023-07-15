f1906212e70fc9aa836969e67e35bd9c8f2da36b
sakthivel ramamoorthi <sakthi@vishgyana.com>
2023-07-15 19:31:55 +0530

    new changes

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -1,12 +1,12 @@
 import axios from 'axios' 
 
 const base = axios.create({
-    baseURL : 'http://localhost:7000/auth'
+    baseURL :   'https://ms2-bn4y.onrender.com/v1'
 })
-
+// 'http://localhost:8080/v1/' ||
  export const login =  async (data:any) => {    
     try {
-        const result = await base.post('/signin', data)
+        const result = await base.post('/login/', data)
         console.log(result.data,"api")
         return result.data.result ? result.data.result : false
     } catch (error) {
